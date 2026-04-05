5b7e2c430b11c63ced9b42be07ea0f5c94225256
t.putintseva145@gmail.com <t.putintseva145@gmail.com>
2026-04-05 15:16:04 +0500

    github

--- github/main.js
+++ github/main.js
@@ -9,11 +9,38 @@ async function getCard() {
     card.classList.add("none");
     load.classList.remove("none");
 
-    const response = await fetch('https://api.github.com/users/KrasnovDaniil1');
+    const response = await fetch(`https://api.github.com/users/${searchValue.value}`);
     if (response.ok) {
         dataCard = await response.json()
         console.log(dataCard)
+        generateCard()
     }
+    load.classList.add("none")
 }
 
 searchBtn.addEventListener("click", getCard)
+
+function generateCard() {
+    card.innerHTML = 
+    `
+    <img src="${dataCard.avatar_url}"/>
+    <h1>${dataCard.login}</h1>
+    <p>${dataCard.bio}</p>
+    <nav>
+        <div>
+            <i class="fas fa-map-marker-alt" style="color: yellow;"></i>
+            <span>${dataCard.location}</span>
+        </div>
+        <div>
+            <i class="fas fa-book" style="color: yellowgreen;"></i>
+            <span>${dataCard.public_repos}</span>
+        </div>
+        <div>
+            <i class="fas fa-heart" style="color: red;"></i>
+            <span>${dataCard.followers}</span>
+        </div>
+    </nav>
+    <a href="${dataCard.url}">Перейти</a>
+    `
+    card.classList.remove("none");
+}
