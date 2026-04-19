12a870cdf3a002a3fbbb01ffd17741c618a886e9
t.putintseva145@gmail.com <t.putintseva145@gmail.com>
2026-04-19 15:24:39 +0500

    paint

--- carusel/main.js
+++ carusel/main.js
@@ -9,12 +9,25 @@ for (let i of carusel.children) {
     i.style.minWidth = `${width}%`
 }
 
-let count_item = carusel.children.length
+let count_item = carusel.children.length - carusel_elem
 let current = 0
 
 btn_right.addEventListener("click", Right)
 
 function Right() {
     current += 1
+    if (current > count_item) {
+        current = 0
+    }
+    carusel.style.transform = `translateX(-${width * current}%)`
+}
+
+btn_left.addEventListener("click", Left)
+
+function Left() {
+    current -= 1
+    if (current < 0) {
+        current = count_item
+    }
     carusel.style.transform = `translateX(-${width * current}%)`
 }
